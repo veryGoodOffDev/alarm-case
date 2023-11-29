2df1030c06476e70c90f30f0e84dfba03085bcf0
unknown <iluha.kot@gmail.com>
2023-11-29 23:29:25 +0300

    prepare structure

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './shared/styles/global.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello React App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Мой рюкзак</h1>
+        </div>
+    )
+}
+
+export default Header;
